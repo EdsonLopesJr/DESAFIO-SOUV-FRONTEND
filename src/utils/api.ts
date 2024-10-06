5a1eb8f7d03f8bce3850e540f916ababb86a87a8
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-06 11:28:21 -0300

    fix: deployment configuration env name

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const Api = axios.create({
-  baseURL: process.env.BaseURL || "http://localhost:3333",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3333",
 });
