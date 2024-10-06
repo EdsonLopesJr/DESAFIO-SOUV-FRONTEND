34c9a5fd89f059d4a91382f8346d25b4665f9c28
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-10-06 10:14:52 -0300

    fix: deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
+    baseUrl: "./src",
+    paths: {
+      "@/*": ["*"],
+    },
   },
   images: {
     domains: ["edsonlopesjr.github.io"],
